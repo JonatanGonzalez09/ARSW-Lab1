5325f12cfad51a844f06bcd236d7a1ce64a4f379
JonatanGonzalez09 <jonatan0924@gmail.com>
2020-01-27 09:56:26 -0500

    Finalizada parte 1

--- DOGS_RACE/CONCURRENT_PROGRAMMING-JAVA_MAVEN-DOGS_RACE/parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ DOGS_RACE/CONCURRENT_PROGRAMMING-JAVA_MAVEN-DOGS_RACE/parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,14 +1,33 @@
 package edu.eci.arsw.primefinder;
 
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
-		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		pft.start();
-		
-		
+	public static void main(String[] args) throws InterruptedException {
+		PrimeFinderThread primHilo=new PrimeFinderThread(0, 10000000);
+		PrimeFinderThread segHilo=new PrimeFinderThread(10000000, 20000000);
+		PrimeFinderThread tercHilo=new PrimeFinderThread(20000000, 30000000);
+		primHilo.start();
+		segHilo.start();
+		tercHilo.start();
+
+		//Pausar los hilos despues de 5 segundos
+		Thread.sleep(5000);
+		primHilo.pausar();
+		segHilo.pausar();
+		tercHilo.pausar();
+		System.out.println("Numero de primos encontrados del primer hilo: "+primHilo.getPrimes().size());
+		System.out.println("Numero de primos encontrados del segundo hilo: "+segHilo.getPrimes().size());
+		System.out.println("Numero de primos encontrados del tercer hilo: "+tercHilo.getPrimes().size());
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Presione la tecla enter para continuar: ");
+		scan.nextLine();
+		scan.close();
+		primHilo.reanudar();
+		segHilo.reanudar();
+		tercHilo.reanudar();
 	}
-	
 }
+
--- DOGS_RACE/CONCURRENT_PROGRAMMING-JAVA_MAVEN-DOGS_RACE/parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ DOGS_RACE/CONCURRENT_PROGRAMMING-JAVA_MAVEN-DOGS_RACE/parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -3,28 +3,35 @@ package edu.eci.arsw.primefinder;
 import java.util.LinkedList;
 import java.util.List;
 
-public class PrimeFinderThread extends Thread{
+public class PrimeFinderThread extends Thread {
 
-	
-	int a,b;
-	
-	private List<Integer> primes=new LinkedList<Integer>();
-	
+	int a, b;
+	boolean pausar;
+
+	private List<Integer> primes = new LinkedList<Integer>();
+
 	public PrimeFinderThread(int a, int b) {
 		super();
 		this.a = a;
 		this.b = b;
 	}
 
-	public void run(){
-		for (int i=a;i<=b;i++){						
-			if (isPrime(i)){
+	public void run() {
+		for (int i = a; i <= b; i++) {
+			if (isPrime(i)) {
 				primes.add(i);
-				System.out.println(i);
+				// System.out.println(i);
+			}
+			synchronized (this) {
+				while (pausar) {
+					try {
+						wait(); ////Fuerza al hilo actual a esperar hasta que algún otro hilo invoca notify() o notifyAll() en el mismo objeto.
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
 			}
 		}
-		
-		
 	}
 	
 	boolean isPrime(int n) {
@@ -39,8 +46,16 @@ public class PrimeFinderThread extends Thread{
 	public List<Integer> getPrimes() {
 		return primes;
 	}
-	
-	
-	
-	
+
+	public void pausar(){
+		pausar=true;
+	}
+
+	public void reanudar(){
+		pausar=false;
+		synchronized(this){
+			notifyAll();
+		}
+	}
+
 }
